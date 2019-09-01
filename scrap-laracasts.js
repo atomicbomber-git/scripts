07f51f4eb198021eeac593d8595d582e8bd88758
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-09-01 20:02:07 +0700

    Updated.

---
+++ scrap-laracasts.js
@@ -0,0 +1,19 @@
+let body = document.querySelectorAll("body")
+let nodes = document.querySelectorAll("ol.episode-list.is-condensed .episode-list-title a")
+let node_array = []
+let base_url = "https://laracasts.com"
+
+for (var i = 0; i < nodes.length; ++i) {
+    node_array.push(nodes[i]);
+}
+
+node_array = node_array.map(async (node) => {
+    let response = await fetch(node.href)
+    let container = document.createElement("div")
+    container.innerHTML = await response.clone().text()
+    let downloadButton = container.querySelector("video-card")
+    return base_url + downloadButton.getAttribute("download-link")
+})
+
+let links = await Promise.all(node_array)
+console.log(links.join("\n"))
