c5ce3d63808551d8e0dfd3c5b6be1b9abc24b0e8
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-03-24 12:35:49 +0700

    Updates.

--- prepend_each_file_with_dirname.py
+++ prepend_each_file_with_dirname.py
@@ -8,18 +8,16 @@ if (len(sys.argv) < 2):
     exit(1)
 
 dir_abs_path = os.path.abspath(sys.argv[1])
-dirname = os.path.dirname(sys.argv[1])
+dirname = os.path.basename(dir_abs_path)
 
 if (not os.path.isdir(dir_abs_path)):
     print("Error: {} is not a directory".format(dir_abs_path) )
     exit(1)
 
 for filename in os.listdir(dir_abs_path):
-    filename = filename.lower().replace(" ", "_")
-
     prefix = dirname.lower().replace(" ", "_") + "_"
     abs_old_filename = os.path.join(dir_abs_path, filename)
 
     if not filename.startswith(prefix):
-        abs_new_filename = os.path.join(dir_abs_path, prefix + filename)
+        abs_new_filename = os.path.join(dir_abs_path, prefix + filename.lower().replace(" ", "_"))
         os.rename(abs_old_filename, abs_new_filename)
